4ab109e2840a7e759adaeeb4dfcc3c15129c102a
werbitskiy <kirverbitskiy@gmail.com>
2021-10-17 02:45:44 +0700

    Код

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account_balance = 2000000000;
+        int transfer = 500000000;
+        int total = account_balance + transfer;
+        System.out.println(total);
+    }
+}
